3f0dbf0a202853c13b03a242bc2f6acb11036977
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-12 14:48:02 +0300

    Updated MainResource interface: - Added getInfo method; - Added JavaDoc;

--- src/main/java/com/qthegamep/jersey/grizzly/example/resource/MainResource.java
+++ src/main/java/com/qthegamep/jersey/grizzly/example/resource/MainResource.java
@@ -1,13 +1,29 @@
 package com.qthegamep.jersey.grizzly.example.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import javax.ws.rs.core.Response;
 
 public interface MainResource {
 
+    /**
+     * This is an example of the root resource.
+     *
+     * @return name of the project.
+     */
     @GET
     @Produces("text/plain; charset=UTF-8")
     Response getMain();
+
+    /**
+     * This is an example of the child resource.
+     *
+     * @return author.
+     */
+    @GET
+    @Path("/info")
+    @Produces("text/plain; charset=UTF-8")
+    Response getInfo();
 }
