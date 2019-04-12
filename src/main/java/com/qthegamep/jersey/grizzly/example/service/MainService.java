d596a70c3361eb721a9f6075515d2a5be42b2a6a
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-12 15:35:55 +0300

    Created MainService interface

---
+++ src/main/java/com/qthegamep/jersey/grizzly/example/service/MainService.java
@@ -0,0 +1,14 @@
+package com.qthegamep.jersey.grizzly.example.service;
+
+public interface MainService {
+
+    /**
+     * @return name of the project.
+     */
+    String getMain();
+
+    /**
+     * @return author.
+     */
+    String getInfo();
+}
